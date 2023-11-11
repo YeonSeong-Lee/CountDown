dc5a4799a632c0ac9d1a1071fc9ca184818c0b12
yeonseong <yeonseong.dev@gmail.com>
2023-11-11 19:44:40 +0900

    chore: delete milesecond

--- js/countdown.js
+++ js/countdown.js
@@ -8,7 +8,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-    const milliseconds = Math.floor((timeLeft % (1000 * 60)) / 100);
     if (timeLeft <= 0) {
       clearInterval(downloadTimer);
       document.getElementById("comment").innerHTML = "Finished";
@@ -17,7 +16,6 @@ document.addEventListener('DOMContentLoaded', function () {
       document.getElementById("hours").innerHTML = hours;
       document.getElementById("minutes").innerHTML = minutes;
       document.getElementById("seconds").innerHTML = seconds;
-      document.getElementById("milliseconds").innerHTML = milliseconds;
     }
   }, 1000);
 }, false);
